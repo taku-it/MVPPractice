069f738bd3a28a37d4b1dddcaa47f0b0380bc501
Takuto Ikuta <58894683+taku-it@users.noreply.github.com>
2022-02-18 18:23:52 +0900

    Initial Commit

---
+++ MVPPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVPPractice
+//
+//  Created by 生田拓登 on 2022/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
